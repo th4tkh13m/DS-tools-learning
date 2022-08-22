15d19a76d091c9a5dd509cae8b82adfedabad101
Khiem <th4tkh13m@protonmail.com>
2022-08-22 10:33:20 +0700

    feat: db_connect docstring, remove db_info

--- sqlalchemy/db_connect.py
+++ sqlalchemy/db_connect.py
@@ -1,13 +1,32 @@
-from sqlalchemy import create_engine
+"""
+This module handles connections to the database.
+"""
+
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 
-from db_info import get_info
 
-sql_type, username, password, host, dbname = get_info()
+def connect(file_path:str) -> Engine:
+    """
+    Connect to a database given a database information file path.
 
-## Creating engine
-engine = create_engine(f"{sql_type}://{username}:{password}@{host}/{dbname}")
+    Args:
+        file_path (str): The relative path of the database information file.
 
-## Check if database exists, else create a new one with the given url
-if not database_exists(engine.url):
-    create_database(engine.url)
+    Returns:
+        Engine: The engine of the connection.
+    """
+    ## Get database information
+    with open(file_path, encoding="utf-8") as info_file:
+        for line in info_file:
+            sql_type, username, password, host, dbname = line.split(";")
+
+    ## Creating engine
+    engine = create_engine(f"{sql_type}://{username}:{password}@{host}/{dbname}")
+
+    ## Check if database exists, else create a new one with the given url
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    return engine
